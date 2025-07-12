398234b21049e97dd3054088b106fa621960946a
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-07-12 17:21:17 -0600

    feat: add FormsModule to enable two-way binding with ngModel

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,6 +1,7 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -29,7 +30,8 @@ import { Inputs } from './shared/inputs/inputs';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
