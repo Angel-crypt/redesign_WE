c662a5c5209d1e9a454807e26efe72c460232a78
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-07-12 17:10:23 -0600

    feat: implement maestro login method with error handling in login component

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MaestroAuthService } from '../../services/maestro/auth/s-auth';
+import { MaestroLoginRequest } from '../../interfaces/maestro-iauth';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './login.less'
 })
 export class Login {
+  constructor(private authService: MaestroAuthService) { }
+  login() {
+    const nCredentials: MaestroLoginRequest = {
+      id_usuario: '',
+      contrasena: '',
+    };
 
+    this.authService.loginMaestro(nCredentials).subscribe({
+      next: (res) => {
+        if (res.success) {
+          console.log('Login correcto:', res.message);
+        } else {
+          console.error('Error:', res.message);
+        }
+      },
+      error: (err) => {
+        if (err.status === 401) {
+          // Aquí capturamos el error 401 y mostramos el mensaje del backend
+          console.error('Error:', err.error.error || 'Credenciales inválidas');
+        } else {
+          console.error('Error en la solicitud HTTP:', err);
+        }
+      }
+    });
+
+  }
 }
