bb84f16e9a37fb9b3a3fd8731d4fd8d33a27eea6
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-07-12 18:18:49 -0600

    feat(router): add landing route with authGuard protection and default redirect to login

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Login } from './pages/login/login'
+import { authGuard } from './services/maestro/auth/guard';
+
+// Rutas de maestro
+import { Landing } from './pages/user/landing/landing';
 
 const routes: Routes = [
-  { path: "login", component: Login}
+  { path: "login", component: Login },
+  { path: "landing", component: Landing, canActivate: [authGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
