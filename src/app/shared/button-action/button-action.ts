ffc2e1e8a73bb1011c1dbb8b6f9a063fccdf29cd
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-07-12 22:28:52 -0600

    feat(button-action): add customizable button component with disabled state and click event

---
+++ src/app/shared/button-action/button-action.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button-action',
+  standalone: false,
+  templateUrl: './button-action.html',
+  styleUrl: './button-action.less'
+})
+export class ButtonAction {
+  @Input() label!: string;
+  @Input() type!: 'button' | 'submit' | 'reset';
+  @Input() disabled: boolean = false;
+
+  @Output() action = new EventEmitter<void>();
+}
