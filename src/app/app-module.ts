da1a9b2841972d9ea8c3c59a974c2ef7cef85fc9
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-07-12 20:15:41 -0600

    feat(app): add layout with conditional sidebar rendering

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,10 +2,14 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Login } from './pages/login/login';
+import { Landing } from './pages/user/landing/landing';
+
 import { EditProfile } from './pages/user/edit-profile/edit-profile';
 import { AssignedGroups } from './pages/user/assigned-groups/assigned-groups';
 import { AdminLanding } from './pages/admin/admin-landing/admin-landing';
@@ -13,6 +17,7 @@ import { AdminUsers } from './pages/admin/admin-users/admin-users';
 import { Navbar } from './shared/navbar/navbar';
 import { Buttons } from './shared/buttons/buttons';
 import { Inputs } from './shared/inputs/inputs';
+import { Sidebar } from './shared/sidebar/sidebar';
 
 @NgModule({
   declarations: [
@@ -25,17 +30,18 @@ import { Inputs } from './shared/inputs/inputs';
     Navbar,
     Buttons,
     Inputs,
-  
+    Landing,
+    Sidebar,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.less'
+  styleUrl: './app.less',
 })
 export class App {
   protected title = 'redesign_WE';
+  constructor(private router: Router) {}
+  isNotLoginPage(): boolean {
+    return this.router.url !== '/login';
+  }
 }
