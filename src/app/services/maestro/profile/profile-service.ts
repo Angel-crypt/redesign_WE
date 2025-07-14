f0307a8c01d8ab6091b1240fb87f3d1a2ecbd3ba
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-07-13 18:58:03 -0600

    feat(services/maestro/profile): implement getPerfilData method in ProfileService

--- src/app/services/maestro/profile/profile-service.ts
+++ src/app/services/maestro/profile/profile-service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap, map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private baseUrl = 'http://localhost:5000';
+  private apiVersion = 'v1';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getPerfilData() {
+    const url = `${this.baseUrl}/${this.apiVersion}/maestro/profile`;
+    return this.http.get(url, { withCredentials: true }).pipe(
+      map((response: any) => response.data),
+      catchError((error) => {
+        console.error('Error fetching profile data:', error);
+        return of(null);
+      })
+    );
+  }
 }
