b3a35d4a4b57435beb444b9a0e1d23adcbe629f5
abdy <abdyn.7@gmail.com>
2025-07-13 22:33:45 -0600

    fix: adapt Assignments and Availability components to work without standalone

--- src/app/pages/user/landing/components/availability/availability.ts
+++ src/app/pages/user/landing/components/availability/availability.ts
@@ -1,15 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common'; // necesario para *ngFor
-import { Availability } from './availability.model';
+import { Availability as AvailabilityModel } from './availability.model';
 
 @Component({
   selector: 'app-availability',
-  standalone: true,
-  imports: [CommonModule], // habilitamos directivas como *ngFor y *ngIf
+  standalone: false,
   templateUrl: './availability.html',
   styleUrls: ['./availability.less']
 })
-export class AvailabilityComponent {
+export class Availability {
   @Input() disponibilidad: Availability[] = [];
   @Input() total: number = 0;
 }
