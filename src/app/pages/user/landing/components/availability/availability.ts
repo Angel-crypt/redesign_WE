ec5e997fef7014fe9fd759a1cc4670208e7c3bbd
abdy <abdyn.7@gmail.com>
2025-07-13 20:56:25 -0600

    fix: import CommonModule to enable *ngFor in availability

--- src/app/pages/user/landing/components/availability/availability.ts
+++ src/app/pages/user/landing/components/availability/availability.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // necesario para *ngFor
 import { Availability } from './availability.model';
 
 @Component({
   selector: 'app-availability',
+  standalone: true,
+  imports: [CommonModule], // habilitamos directivas como *ngFor y *ngIf
   templateUrl: './availability.html',
   styleUrls: ['./availability.less']
 })
