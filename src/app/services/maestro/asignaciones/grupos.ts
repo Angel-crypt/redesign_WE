f3a7a1840d48c51bebfea7299f44b3b4cf62a811
Angel-crypt <angel.cruz.030905@gmail.com>
2025-07-14 20:09:35 -0600

    feat(grupos): initialize Grupos service with HttpClient and necessary imports

--- src/app/services/maestro/asignaciones/grupos.ts
+++ src/app/services/maestro/asignaciones/grupos.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Asignacion, Grupo, Curso, Horario } from '../../../interfaces/entities';
+import { MaestroAsignacionesResponse } from '../../../interfaces/maestro-interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Grupos {
+  private baseUrl = 'http://localhost:5000';
+  private apiVersion = 'v1';
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 }
