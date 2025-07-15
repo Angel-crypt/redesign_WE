a9d49440d6bfff98cc896a26b862b2c42e94b621
Angel-crypt <angel.cruz.030905@gmail.com>
2025-07-14 19:12:04 -0600

    refactor(disponibilidad-service): improve error handling and code formatting

--- src/app/services/maestro/profile/disponibilidad-service.ts
+++ src/app/services/maestro/profile/disponibilidad-service.ts
@@ -1,35 +1,54 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, throwError, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-
 import { Disponibilidad } from '../../../interfaces/entities';
 import { MaestroDisponibilidadResponse } from '../../../interfaces/maestro-interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DisponibilidadService {
   private baseUrl = 'http://localhost:5000';
   private apiVersion = 'v1';
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {}
 
   getDisponibilidad(): Observable<MaestroDisponibilidadResponse> {
     const url = `${this.baseUrl}/${this.apiVersion}/maestro/availability`;
-    return this.http.get<MaestroDisponibilidadResponse>(url, { withCredentials: true }).pipe(
-      catchError(error => {
-        console.error('Error fetching disponibilidad:', error);
-        return of({} as MaestroDisponibilidadResponse);
-      })
-    );
+    return this.http
+      .get<MaestroDisponibilidadResponse>(url, { withCredentials: true })
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching disponibilidad:', error);
+          return of({} as MaestroDisponibilidadResponse);
+        })
+      );
   }
-  updateDisponibilidad(data: Disponibilidad): Observable<MaestroDisponibilidadResponse> {
+
+  updateDisponibilidad(
+    data: Disponibilidad
+  ): Observable<MaestroDisponibilidadResponse> {
     const url = `${this.baseUrl}/${this.apiVersion}/maestro/availability`;
-    return this.http.put<MaestroDisponibilidadResponse>(url, data, { withCredentials: true }).pipe(
-      catchError(error => {
-        console.error('Error updating disponibilidad:', error);
-        return of({} as MaestroDisponibilidadResponse);
-      })
-    );
+    return this.http
+      .put<MaestroDisponibilidadResponse>(url, data, { withCredentials: true })
+      .pipe(
+        catchError((error) => {
+          return throwError(() => error);
+        })
+      );
+  }
+
+  createDisponibilidad(
+    data: Disponibilidad
+  ): Observable<MaestroDisponibilidadResponse> {
+    const url = `${this.baseUrl}/${this.apiVersion}/maestro/availability`;
+    return this.http
+      .post<MaestroDisponibilidadResponse>(url, data, { withCredentials: true })
+      .pipe(
+        catchError((error) => {
+          return throwError(() => error);
+        })
+      );
   }
 }
