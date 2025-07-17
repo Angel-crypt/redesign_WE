ea13bc2b2d4a4b19faa1057c93cc7687d07d695c
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-07-17 00:04:27 -0600

    feat(maestros): implement SMaestros service with getAllMaestros method for fetching maestro data

--- src/app/services/admin/maestros/s-maestros.ts
+++ src/app/services/admin/maestros/s-maestros.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { AdminMaestroResponse } from '../../../interfaces/admin-interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SMaestros {
+  private baseUrl = 'http://localhost:5000';
+  private apiVersion = 'v1';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllMaestros(): Observable<AdminMaestroResponse> {
+    const url = `${this.baseUrl}/${this.apiVersion}/admin/maestros`;
+    return this.http.get<AdminMaestroResponse>(url, { withCredentials: true }).pipe(
+      catchError(error => {
+        console.error('Error fetching maestros:', error);
+        return of({} as AdminMaestroResponse);
+      })
+    );
+  }
 }
