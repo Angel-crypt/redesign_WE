d4ed21864123b88269d3f0c7daa331b1f480587b
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-07-17 00:41:02 -0600

    feat(data-maestro): implement DataMaestro component with template, styles, and age calculation

--- src/app/pages/admin/admin-maestros/components/data-maestro/data-maestro.ts
+++ src/app/pages/admin/admin-maestros/components/data-maestro/data-maestro.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Maestro } from '../../../../../interfaces/entities';
 
 @Component({
   selector: 'app-data-maestro',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './data-maestro.less'
 })
 export class DataMaestro {
+  @Input() maestro!: Maestro;
 
+  edadMaestro = 0;
+  get edad(): number {
+    if (this.maestro.fecha_nacimiento) {
+      const fechaNacimiento = new Date(this.maestro.fecha_nacimiento);
+      const hoy = new Date();
+      this.edadMaestro = hoy.getFullYear() - fechaNacimiento.getFullYear();
+      const mes = hoy.getMonth() - fechaNacimiento.getMonth();
+      if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNacimiento.getDate())) {
+        this.edadMaestro--;
+      }
+    }
+    return this.edadMaestro;
+  }
 }
